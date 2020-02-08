555a231663b629bcbf0f819bfa73c6e2fade782d
Dark-Existed <c2572388311@gmail.com>
2020-02-08 16:43:26 +0800

    fix: 使用cloneDocumentFromReader 导致 ValueError 的问题

--- merge_pdf.py
+++ merge_pdf.py
@@ -20,7 +20,8 @@ def add_bookmarks(path, file_dir, data=None):
         bookmarks = data['Data']
     book = PdfFileReader(path, strict=False)
     pdf = PdfFileWriter()
-    pdf.cloneDocumentFromReader(book)
+    for i in range(book.getNumPages()):
+        pdf.addPage(book.getPage(i))
     mark_list = []
     for bookmark in bookmarks:
 
@@ -30,66 +31,7 @@ def add_bookmarks(path, file_dir, data=None):
                 if mark['id'] == bookmark['ParentId']:
                     parent = mark['bookmark']
                     break
-        mark = pdf.addBookmark(bookmark['Title'], bookmark['Page'] - 1, parent=parent)
-        mark_list.append({
-            'id': bookmark['Id'],
-            'bookmark': mark
-        })
-
-    with open(path[0:path.rfind('.')] + '.bookmark.pdf', 'wb') as fout:
-        try:
-            pdf.write(fout)
-        except:
-            print(path[0:path.rfind('.')] + "格式有误")
-            pass
-
-
-def file_name_walk(file_dir):
-    for root, dirs, files in os.walk(file_dir):
-        if 'bookmark.json' in files:
-            files.remove('bookmark.json')
-        files.sort(key=lambda x: int(x[x.rfind('-') + 1:][:-4]))
-        file_list = [file_dir + '\\' + file for file in files]
-        merger = PdfFileMerger(strict=False)
-        for pdf in file_list:
-            merger.append(pdf)
-        path = files[0][:files[0].rfind('-')] + '.pdf'
-        merger.write(path)
-        add_bookmarks(path, file_dir)
-
-#!/usr/bin/env python
-# coding:utf-8
-from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
-import os
-import sys
-import json
-import requests
-import urllib
-import random
-import time
-
-deviceToken = 'ebook9D572D5B4B8F3BEC28589B10B07F8EC2'
-
-
-def add_bookmarks(path, file_dir, data=None):
-    if not data:
-        with open(file_dir + '\\bookmark.json', 'rb') as f:
-            bookmarks = json.load(f)['Data']
-    else:
-        bookmarks = data['Data']
-    book = PdfFileReader(path, strict=False)
-    pdf = PdfFileWriter()
-    pdf.cloneDocumentFromReader(book)
-    mark_list = []
-    for bookmark in bookmarks:
-
-        parent = None
-        if bookmark['ParentId'] != 0:
-            for mark in mark_list:
-                if mark['id'] == bookmark['ParentId']:
-                    parent = mark['bookmark']
-                    break
-        mark = pdf.addBookmark(bookmark['Title'], bookmark['Page'] - 1, parent=parent)
+        mark = pdf.addBookmark(bookmark['Title'], bookmark['Page'], parent=parent)
         mark_list.append({
             'id': bookmark['Id'],
             'bookmark': mark
